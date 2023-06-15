589e9a86d6a9d9320ff7ddeec2bcef918df7ed4b
Nadolski, Maikel <maikel.nadolski@gmail.com>
2023-06-15 09:52:57 +0200

    specify tcp::v4 in test

--- source/sio/net/ip/address.hpp
+++ source/sio/net/ip/address.hpp
@@ -48,28 +48,28 @@ namespace sio::net::ip {
 
     address_v4() = default;
 
-    explicit address_v4(uint_type __addr) noexcept
-      : __addr_{::htonl(__addr)} {
+    explicit address_v4(uint_type addr) noexcept
+      : addr_{::htonl(addr)} {
     }
 
-    explicit address_v4(bytes_type __bytes) noexcept
-      : __addr_{std::bit_cast<::in_addr_t>(__bytes)} {
+    explicit address_v4(bytes_type bytes) noexcept
+      : addr_{std::bit_cast<::in_addr_t>(bytes)} {
     }
 
     uint_type to_uint() const noexcept {
-      return ::ntohl(__addr_);
+      return ::ntohl(addr_);
     }
 
     bytes_type to_bytes() const noexcept {
-      return std::bit_cast<bytes_type>(__addr_);
+      return std::bit_cast<bytes_type>(addr_);
     }
 
     template <class _Allocator = std::allocator<char>>
     std::basic_string<char, std::char_traits<char>, _Allocator>
       to_string(_Allocator __alloc = _Allocator()) const {
       char __buffer[INET_ADDRSTRLEN + 1] = {};
-      inet_ntop(AF_INET, &__addr_, __buffer, INET_ADDRSTRLEN);
-      return std::string(__buffer, __alloc);
+      inet_ntop(AF_INET, &addr_, __buffer, INET_ADDRSTRLEN);
+      return std::basic_string<char, std::char_traits<char>, _Allocator>(__buffer, __alloc);
     }
 
     bool is_loopback() const noexcept {
@@ -83,7 +83,7 @@ namespace sio::net::ip {
     friend auto operator<=>(const address_v4&, const address_v4&) = default;
 
    private:
-    ::in_addr_t __addr_;
+    ::in_addr_t addr_;
   };
 
   class address_v6 {
@@ -100,23 +100,26 @@ namespace sio::net::ip {
 
     address_v6() noexcept;
 
-    explicit address_v6(const bytes_type& __bytes, unsigned long __scope_id = 0) {
+    explicit address_v6(const bytes_type& bytes, unsigned long __scope_id = 0) {
     }
 
     unsigned long scope_id() const noexcept {
-      return __scope_id_;
+      return scope_id_;
     }
 
     void scope_id(unsigned long id) noexcept {
-      __scope_id_ = id;
+      scope_id_ = id;
     }
 
     bytes_type to_bytes() const noexcept {
-      return std::bit_cast<bytes_type>(__addr_);
+      return std::bit_cast<bytes_type>(addr_);
     }
 
-    std::string to_string() const {
-      return "";
+    template <class _Allocator = std::allocator<char>>
+    std::string to_string(_Allocator alloc = _Allocator()) const {
+      char buffer[INET6_ADDRSTRLEN + 1] = {};
+      inet_ntop(AF_INET6, &addr_, buffer, INET6_ADDRSTRLEN);
+      return std::basic_string<char, std::char_traits<char>, _Allocator>(buffer);
     }
 
     bool is_loopback() const noexcept;
@@ -159,10 +162,10 @@ namespace sio::net::ip {
 
    private:
     // The underlying IPv6 address.
-    in6_addr_type __addr_;
+    in6_addr_type addr_;
 
     // The scope ID associated with the address.
-    unsigned long __scope_id_;
+    unsigned long scope_id_;
   };
 
   class address {
@@ -170,29 +173,29 @@ namespace sio::net::ip {
     address() = default;
 
     address(address_v4 __other) noexcept
-      : __addr_(__other) {
+      : addr_(__other) {
     }
 
     address(address_v6 __other) noexcept
-      : __addr_(__other) {
+      : addr_(__other) {
     }
 
     bool is_v4() const noexcept {
-      return std::holds_alternative<address_v4>(__addr_);
+      return std::holds_alternative<address_v4>(addr_);
     }
 
     bool is_v6() const noexcept {
-      return std::holds_alternative<address_v6>(__addr_);
+      return std::holds_alternative<address_v6>(addr_);
     }
 
     /// Get the address as an IP version 4 address.
     address_v4 to_v4() const {
-      return std::get<address_v4>(__addr_);
+      return std::get<address_v4>(addr_);
     }
 
     /// Get the address as an IP version 6 address.
     address_v6 to_v6() const {
-      return std::get<address_v6>(__addr_);
+      return std::get<address_v6>(addr_);
     }
 
     /// Get the address as a string.
@@ -234,7 +237,7 @@ namespace sio::net::ip {
     friend bool operator==(const address&, const address&) = default;
 
    private:
-    std::variant<address_v4, address_v6> __addr_{};
+    std::variant<address_v4, address_v6> addr_{};
   };
 
   class endpoint {
@@ -253,15 +256,15 @@ namespace sio::net::ip {
       }
     }
 
-    explicit endpoint(net::ip::address __addr, unsigned short __port) noexcept {
-      if (__addr.is_v4()) {
+    explicit endpoint(net::ip::address addr, unsigned short __port) noexcept {
+      if (addr.is_v4()) {
         __data_.__v4.sin_family = AF_INET;
         __data_.__v4.sin_port = ::htons(__port);
-        __data_.__v4.sin_addr = bit_cast<::in_addr>(__addr.to_v4().to_bytes());
+        __data_.__v4.sin_addr = bit_cast<::in_addr>(addr.to_v4().to_bytes());
       } else {
         __data_.__v6.sin6_family = AF_INET6;
         __data_.__v6.sin6_port = ::htons(__port);
-        __data_.__v6.sin6_addr = bit_cast<::in6_addr>(__addr.to_v6().to_bytes());
+        __data_.__v6.sin6_addr = bit_cast<::in6_addr>(addr.to_v6().to_bytes());
       }
     }
 
