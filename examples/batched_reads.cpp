cecc98800b21b5da3596bf8945cb0c8b1fcbb6fc
Maikel Nadolski <mnadolski@psi.de>
2023-09-04 14:40:15 +0200

    Fix fork algorithm when completing with set_value

--- examples/batched_reads.cpp
+++ examples/batched_reads.cpp
@@ -15,20 +15,50 @@
 #include <cassert>
 #include <chrono>
 
-int main(int argc, char *argv[]) {
-  const std::size_t num_ints = argc >= 3 ? std::stoul(argv[2]) : 100000000;
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/unistd.h>
+
+auto get_block_size(int fd) {
+  struct stat fstat;
+  ::fstat(fd, &fstat);
+  auto blksize = fstat.st_blksize;
+  return blksize;
+}
+
+struct aligned_deleter {
+  void operator()(void* ptr) const noexcept {
+    ::free(ptr);
+  }
+};
+
+std::unique_ptr<int[], aligned_deleter>
+  make_unique_aligned(std::size_t size, std::size_t alignment) {
+  void* ptr = nullptr;
+  if (::posix_memalign(&ptr, alignment, sizeof(int) * size) != 0) {
+    throw std::bad_alloc{};
+  }
+  return std::unique_ptr<int[], aligned_deleter>{static_cast<int*>(ptr)};
+}
+
+int main(int argc, char* argv[]) {
+  const std::size_t num_ints_tmp = argc >= 3 ? std::stoul(argv[2]) : 100000000;
   const std::size_t num_reads = argc >= 4 ? std::stoul(argv[3]) : 1000000;
   {
     exec::safe_file_descriptor fd{::memfd_create("test", 0)};
     if (argc >= 5) {
       fd = exec::safe_file_descriptor{::open(argv[4], O_TMPFILE | O_RDWR, S_IRUSR | S_IWUSR)};
     }
+    auto block_size = get_block_size(fd);
+    const std::size_t num_ints =
+      (num_ints_tmp + (block_size / sizeof(int)) - 1)
+      - (num_ints_tmp + (block_size / sizeof(int)) - 1) % (block_size / sizeof(int));
     if (!fd) {
       std::cerr << "file open unsuccessful: " << std::strerror(errno) << '\n';
       return -1;
     }
     {
-      auto a = std::make_unique_for_overwrite<int[]>(num_ints);
+      auto a = make_unique_aligned(num_ints, block_size);
       std::iota(a.get(), a.get() + num_ints, 0);
 
       const auto file_size = sizeof(a.get()[0]) * num_ints;
@@ -53,21 +83,22 @@ int main(int argc, char *argv[]) {
     std::vector<std::size_t> offsets, lens;
     std::mt19937_64 gen(1e9 + 7);
     constexpr int item_size = sizeof(int);
-    constexpr int max_num_ints = 512;
     {
-      std::uniform_int_distribution<std::size_t> off_dist(0, num_ints - 1);
-      std::uniform_int_distribution<std::size_t> ints_dist(1, max_num_ints);
+      int int_block_size = block_size / item_size;
+      int num_int_blocks = num_ints / int_block_size;
+      std::uniform_int_distribution<std::size_t> off_dist(0, num_int_blocks - 1);
       for (int64_t i = 0; i < num_reads; i++) {
-        offsets.push_back(off_dist(gen));
-        const auto num_reads = ints_dist(gen);
-        lens.push_back(std::min(num_ints - offsets.back(), num_reads));
+        offsets.push_back(off_dist(gen) * block_size);
+        const int rest = std::max<int>(0, num_ints - offsets.back());
+        const int length = std::min<int>(rest, int_block_size);
+        lens.push_back(length);
         byte_offsets.push_back(offsets.back() * item_size);
       }
     }
     lens.push_back(0);
     std::exclusive_scan(lens.begin(), lens.end(), lens.begin(), 0);
     const auto total_len = lens.back();
-    auto buffer = std::make_unique_for_overwrite<int[]>(total_len);
+    auto buffer = make_unique_aligned(total_len, block_size);
     auto buffer_data = buffer.get();
     std::vector<std::span<std::byte>> buffers;
     for (int64_t i = 0; i < num_reads; i++)
@@ -92,11 +123,11 @@ int main(int argc, char *argv[]) {
         }
     }
     const auto avg_time = std::accumulate(times.begin(), times.end(), 0.0) / times.size();
-    std::cout
-      << "Read " << num_reads << " blocks of sizes upto " << max_num_ints * item_size
-      << " bytes in time " << avg_time << "s for an average of " << num_reads / avg_time << " IOPS"
-      << " and an average copy rate of " << total_len * item_size / avg_time / (1 << 30) << " GiB/s"
-      << std::endl;
+    const auto n_iops = num_reads / avg_time;
+    const auto n_bytes = n_iops * block_size;
+    std::cout << "Read " << num_reads << " blocks of size " << block_size << " bytes in time "
+              << avg_time << "s for an average of " << num_reads / avg_time << " IOPS"
+              << " and an average copy rate of " << n_bytes / (1 << 30) << " GiB/s" << std::endl;
   }
   return 0;
 }
