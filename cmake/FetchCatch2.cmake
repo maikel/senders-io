b389b2816bc41c9234c0d3863411b38b98c11bac
xiaomingZhang2020 <xiaomingZhang2020@outlook.com>
2024-08-10 10:54:50 +0800

    use FetchContent tool to download thirdparties

---
+++ cmake/FetchCatch2.cmake
@@ -0,0 +1,8 @@
+include(FetchContent)
+FetchContent_Declare(
+  Catch2
+  GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+  GIT_TAG 8898cc61601af6cdd5b4548dd91ccf70ed67f3c2
+)
+FetchContent_MakeAvailable(Catch2)
+
