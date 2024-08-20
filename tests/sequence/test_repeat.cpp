b36a087302badb9ba8d1e062c611bb208ee11187
xiaomingZhang2020 <xiaomingZhang2020@outlook.com>
2024-08-20 22:47:35 +0800

    fix comments: add and update name in copyright, refine message

--- tests/sequence/test_repeat.cpp
+++ tests/sequence/test_repeat.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2024 Maikel Nadolski
- * Copyright (c) 2024 Xiaoming Zhang
+ * Copyright (c) 2024 Emmett Zhang
  *
  * Licensed under the Apache License Version 2.0 with LLVM Exceptions
  * (the "License"); you may not use this file except in compliance with
