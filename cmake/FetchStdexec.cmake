1fbd4e52c5ba131698af075a2167a16d41624c74
John Eivind Helset <private@jehelset.no>
2025-05-01 23:42:14 +0200

    Bump stdexec commit.

--- cmake/FetchStdexec.cmake
+++ cmake/FetchStdexec.cmake
@@ -3,8 +3,8 @@ FetchContent_Declare(
   stdexec
   GIT_REPOSITORY https://github.com/NVIDIA/stdexec.git
 
-  # branch: main. date: 2024-08-13
-  GIT_TAG 54b38c99e8cbb348c1dd3508fa540728882247db
+  # branch: main. date: 2025-04-28
+  GIT_TAG b0b18b82e9a9166af7a51bceeb2d7229f7bef33d
 )
 
 set(STDEXEC_BUILD_EXAMPLES OFF CACHE BOOL "close stdexec examples")
